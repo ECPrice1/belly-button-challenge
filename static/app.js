119813f4afc3008801fa8cefdafed58e056089f1
ECPrice1 <ericprice@Erics-MacBook-Pro.local>
2024-06-04 08:46:16 -0600

    app.js final

--- static/app.js
+++ static/app.js
@@ -1,59 +1,72 @@
 
 // define url
+
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json"
 
 //Fetch the JSON data and console log it
+
 d3.json(url).then(function(data) {
     console.log(data);
 });
 
 // Build the metadata panel
+
 function buildMetadata(sample) 
 {
   d3.json(url).then((data) => 
   {
     // get the metadata field
+    
     let metaData = data.metadata
 
     // Filter the metadata for the object with the desired sample number
+    
     let info = metaData.filter(attribute => attribute.id == sample)
 
     // Use d3 to select the panel with id of `#sample-metadata`
+    
     let metaDatapanel = d3.select("#sample-metadata")
 
     // Use `.html("") to clear any existing metadata
+    
     metaDatapanel.html("")
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    
     info.forEach((attribute) => 
     {
-      Object.entries(attribute).forEach(([key, value]) => 
+      Object.entries(attribute).forEach(([key,value]) => 
       {
-        metaDatapanel.append("p").text(`${key}: ${value}`)
-        console.log(key, value);
+        metaDatapanel.append("p").text(`${key}:${value}`)
+        console.log(key,value);
       });
     });
   })
 }
 
 // function to build both charts
+
 function buildCharts(sample) 
 {
   d3.json(url).then((data) => 
   {
     // Get the samples field
+    
     let samples = data.samples
 
     // Filter the samples for the object with the desired sample number
+    
     let info = samples.filter(attribute => attribute.id === sample)[0]
     
     // Get the otu_ids, otu_labels, and sample_values
+    
     let otu_ids = info.otu_ids
     let otu_labels = info.otu_labels
     let sample_values = info.sample_values
     
     // Build a Bubble Chart
+    
     let trace2 = 
     {
       x: otu_ids,
@@ -73,18 +86,21 @@ function buildCharts(sample)
       }
     
     // Render the Bubble Chart
+    
     Plotly.newPlot("bubble" , bubbleChart, bubbleTitle)
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-      otu_ids = otu_ids.map(id => "OTU " + String(id))
+    
+    otu_ids = otu_ids.map(id => "OTU " + String(id))
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    
     let sortValues = sample_values.sort((a,b) => b.sample_values - a.sample_values);
     let sliceValues = sortValues.slice(0,10)
     sliceValues.reverse()
     
-    // sort and slice
+    
     let trace1 = 
     {
       x: sliceValues,
@@ -93,7 +109,9 @@ function buildCharts(sample)
       type: "bar" ,
       orientation: "h"
     }
+    
     let barChart = [trace1]
+    
     let barLayout = 
     {
       title: "Top 10 Bacteria Cultures Found",
@@ -111,6 +129,7 @@ function buildCharts(sample)
 }
 
 // Function to run on page load
+
 function init() 
 {
   d3.json(url).then((data) => 
@@ -138,12 +157,14 @@ function init()
 }
 
 // Function for event listener
+
 function newSelection(newSample) 
+// Build charts and metadata panel each time a new sample is selected
 {
-  // Build charts and metadata panel each time a new sample is selected
 buildCharts(newSample)
 buildMetadata(newSample)
 }
 
 // Initialize the dashboard
+
 init();
